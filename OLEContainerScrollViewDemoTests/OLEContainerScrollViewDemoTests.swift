6a34c5e5e0fd954df0a4a7d86ab500f3809654fa
Chanon Purananunaka <chanon.pur@outlook.com>
2020-10-21 11:44:16 +0700

    feat(master): add all files

---
+++ OLEContainerScrollViewDemoTests/OLEContainerScrollViewDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  OLEContainerScrollViewDemoTests.swift
+//  OLEContainerScrollViewDemoTests
+//
+//  Created by Chanon Purananunak on 21/10/2563 BE.
+//  Copyright © 2563 Chanon Purananunak. All rights reserved.
+//
+
+import XCTest
+@testable import OLEContainerScrollViewDemo
+
+class OLEContainerScrollViewDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
